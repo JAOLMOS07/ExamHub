31d5e573e740cc99d54950eae047ffafe1cdc63b
JAOLMOS07 <jhogerolmos10@gmail.com>
2024-08-15 20:28:00 -0500

    ajustes generales producción

--- src/app/core/services/pdfService.service.ts
+++ src/app/core/services/pdfService.service.ts
@@ -1,41 +1,18 @@
 import { Injectable } from "@angular/core";
 import { TDocumentDefinitions } from "pdfmake/interfaces";
-
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 type PDFMake = typeof import("pdfmake/build/pdfmake");
 
 @Injectable({ providedIn: "root" })
 export class PDFService {
-  private pdfMake!: PDFMake;
-  private fonts!: { [file: string]: string };
+  private pdfMake: PDFMake = pdfMake;
 
-  async loadPDFMaker() {
-    if (!this.pdfMake) {
-      this.pdfMake = await import("pdfmake/build/pdfmake");
-      (window as any).pdfMake.vfs = (
-        await import("pdfmake/build/vfs_fonts")
-      ).pdfMake.vfs;
-    }
-  }
-
-  async open(def: TDocumentDefinitions) {
-    if (!this.pdfMake) {
-      try {
-        await this.loadPDFMaker();
-      } catch (error) {
-        console.error("Failed to load pdf maker lib");
-      }
-    }
+  open(def: TDocumentDefinitions) {
     this.pdfMake.createPdf(def).open();
   }
   async download(def: TDocumentDefinitions, name: string) {
-    if (!this.pdfMake) {
-      try {
-        await this.loadPDFMaker();
-      } catch (error) {
-        console.error("Failed to load pdf maker lib");
-      }
-    }
-
     this.pdfMake.createPdf(def).download(name);
   }
 }
